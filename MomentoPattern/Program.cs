8e16f37511858f0e2a578bac9943415b6531cf9c
Dzmitry.Shchamialiou <Dzmitry.Shchamialiou@itechart-group.com>
2019-08-27 12:19:39 +0300

    Add project files.

---
+++ MomentoPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MomentoPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new Student();
+            Console.WriteLine($"Initial student status {student.Status}");
+            try
+            {
+                student.StartAdmissionProcess();
+                Console.WriteLine($"Student status after proucessing {student.Status}");
+                throw new Exception();
+            }
+            catch (Exception)
+            {
+                student.Revert();
+            }
+
+            Console.WriteLine($"Student status after failure {student.Status}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MomentoPattern/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MomentoPattern
+{
+    public class Student
+    {
+        public string Status { get; set; }
+        private Memento memento;
+
+        public Student()
+        {
+            this.Status = "Pending Approval";
+            this.memento = new Memento(this.Status);
+        }
+
+        public void StartAdmissionProcess()
+        {
+            this.Status = "Admitted";
+        }
+
+        public void Revert()
+        {
+            this.Status = this.memento.status;
+        }
+    }
+
+    class Memento
+    {
+        public readonly string status;
+
+        public Memento(string status)
+        {
+            this.status = status;
+        }
+    }
+}
